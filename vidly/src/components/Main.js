2134cfadf295c8cb8f1acaa3ef90db942b6e2a9e
saakeeb <sakib14april@gmail.com>
2022-10-05 17:47:54 +0600

    Feature: condional rendering

--- vidly/src/components/Main.js
+++ vidly/src/components/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Movies from './Movies/Movies';
+import './global/main.css'
 
 const Main = () => {
     return (
-        <div>
+        <div className='main'>
             <Movies />
         </div>
     );
--- vidly/src/components/Movies/Movies.js
+++ vidly/src/components/Movies/Movies.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { getMovies } from '../../services/fakeMovieService';
 
 const Movies = () => {
-    const movies = getMovies();
+    const [movies, setMovies] = useState(getMovies());
+
+    const HandleDeleteMovie = (movie) => {
+        const newMovies = movies.filter(m => m._id !== movie._id);
+        setMovies(newMovies);
+    }
+
     return (
-        <Table hover>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    movies.map(movie => 
-                        <tr key={movie._id}>
-                            <td>{movie.title}</td>
-                            <td>{movie.genre.name}</td>
-                            <td>{movie.numberInStock}</td>
-                            <td>{movie.dailyRentalRate}</td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </Table>
+        <>
+            {
+                movies.length ? (
+                    <>
+                        <h3>There {movies.length > 1 ? 'are ' + movies.length + ' movies' : 'is ' + movies.length + ' movie'} in the database.</h3>
+                        <Table hover>
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Genre</th>
+                                    <th>Stock</th>
+                                    <th>Rate</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    movies.map(movie =>
+                                        <tr key={movie._id}>
+                                            <td>{movie.title}</td>
+                                            <td>{movie.genre.name}</td>
+                                            <td>{movie.numberInStock}</td>
+                                            <td>{movie.dailyRentalRate}</td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-danger btn-sm"
+                                                    onClick={() => HandleDeleteMovie(movie)}
+                                                >
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </Table>
+                    </>
+                ) : (
+                     <h3>There is no movies in the database.</h3>
+                )
+            }
+        </>
+        
     );
 };
 
